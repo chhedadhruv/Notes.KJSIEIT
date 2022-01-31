7d13408e61edcf3a01cb7597596bc86a40da5e35
Akshit Savaliya <50403153+AGSAVALIYA@users.noreply.github.com>
2022-01-31 09:57:49 +0530

    content change without reload

---
+++ script.js
@@ -0,0 +1,176 @@
+function notes(){
+    document.getElementById("incont").innerHTML = `<div><div class="div1">
+    <section class="link-light">Physics</section><hr class="link-light">
+    <ul class="link-light">
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1V5wRkhgKh9sEf6R8RArPMHrlJfKFTSC6?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Quantum Physics</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1BDKgMDrF1wQqxW6EGGSRr_eUzh_YFqn4?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Crystallography</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1lLnt3pi8zZUP2HdxHc1Q9dI1Jfc-B4RT?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Syllabus and Question Bank</a></li>
+    </ul>
+  </div>
+  <div class="div2">
+    <section class="link-light">Maths</section><hr class="link-light">
+    <ul class="link-light">
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1lh6Ki_kKg_QndqQN9b06bXhAhfV1D8u-?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Complex Numbers</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1rv7U0p8zRhIacvPgpX9cxbu2oKRCXx4s?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Matrices</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1ndT1cR0nnNlNaDLKHwh50h57eJdMtiif?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Books</a></li>
+    </ul>
+  </div>
+  <div class="div3">
+    <section class="link-light">Chemistry</section><hr class="link-light">
+    <ul class="link-light">
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1RUFrYBNQytHsnamd4cdTcvNcZzqIzfg9?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Corrosion</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1r3dMLFZS4mWsDBLqeXD3CtRdRojkB13Q?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Water</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1TTnVaJeISv0gRBYjSbD-7l71SqT-S--w?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Books</a></li>
+    </ul>
+  </div>
+  <div class="div4">
+    <section class="link-light">BEE</section><hr class="link-light">
+    <ul class="link-light">
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1ZPnFxqi8nHDazYN_4A6OVpci0dOlvB3H?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Lecture Notes</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1Mr_1JMDy4zAiYL2LSSxusmRs6TpS-7Y8?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Books</a></li>
+      <li><a style="text-decoration:none;" href="https://drive.google.com/file/d/15QaD5CrwGe1LKIDsDb0ZuxQcloyvfH1r/view?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Question Bank</a></li>
+    </ul>
+  </div>
+  <div class="div5">
+    <section class="link-light">Mechanics</section><hr class="link-light">
+    <ul class="link-light">
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1k2QcwkcGcMnOYMLbsPLbgJLLo0fd96Xq?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Forces</a></li>
+      <li><a style="text-decoration: none;" href="https://drive.google.com/drive/folders/1i6YhADhun7FOluMCJ2uMqiVWTYBmlCe5?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Centroid</a></li>
+      <li><a style="text-decoration:none;" href="https://drive.google.com/file/d/1ymBpytuvyBoJE36I0vHYV1x94tcalWDa/view?usp=sharing" class="link-light" target="_blank" rel="noreferre noopener">Question Bank</a></li>
+    </ul>
+  </div><div>`;
+  document.getElementById("pagestyle").setAttribute('href', 'assets/notes.css');
+}
+
+function lectures(){
+    document.getElementById("incont").innerHTML = `<div class="div1"><h1>Lectures</h1></div>
+    <div class="div2">
+        <section>Physics</section><hr>
+        <ol>
+            <li>Quantum Physics</li>
+            <ul>
+                <li><a style="text-decoration: none;" href="https://youtu.be/R3YIlcCZWxQ" class="link-light">Quantum Physics Session 1</a></li>
+                <li><a style="text-decoration: none;" href="https://youtu.be/TBDrutCvKN4" class="link-light">Quantum Physics Session 2</a></li>
+                <li><a style="text-decoration: none;" href="https://youtu.be/nC_OAaG7T50" class="link-light">Quantum Physics Session 3</a></li>
+                <li><a style="text-decoration: none;" href="https://youtu.be/caAPH4RT_Ec" class="link-light">Quantum Physics Session 4</a></li>
+                <li><a style="text-decoration: none;" href="https://youtu.be/P6YnCbvvvHY" class="link-light">Quantum Physics Session 5</a></li>
+                <li><a style="text-decoration: none;" href="https://youtu.be/EtNBK8uoFDY" class="link-light">Quantum Physics Session 6</a></li>
+                <li><a style="text-decoration: none;" href="https://youtu.be/4Lvnk0FwHEo" class="link-light">Quantum Physics Session 7</a></li>
+            </ul><hr>
+        <li>Crystallography</li>
+        <ul>
+          <li><a style="text-decoration:none;" href="https://youtu.be/wRgrsfZaeAg" class="link-light">Unit cell, Space lattice, Crystal structure</a></li>
+          <li><a style="text-decoration:none;" href="https://youtu.be/3LF3g_iIsPk" class="link-light">Miller Indices</a></li>
+          <li><a style="text-decoration:none;" href="https://youtu.be/3avAmNMV7JA" class="link-light">Determination of crystal structure using X ray diffraction</a></li>
+        </ul><hr>
+        <li>Semiconductor Physics</li>
+        <ul>
+          <li><a style="text-decoration:none;" href="https://youtu.be/CXFtbpYaBXw" class="link-light">Formation of Energy bands</a></li>
+          <li><a style="text-decoration:none;" href="https://youtu.be/6ShYjai83bA" class="link-light">Fermi Level and Fermi Function</a></li>
+          <li><a style="text-decoration:none;" href="https://youtu.be/67QXoPW0eE0" class="link-light">Conductivity, current density and mobility</a></li>
+          <li><a style="text-decoration:none;" href="https://youtu.be/5iEsqeMOZAc" class="link-light">Hall Effect</a></li>
+          <li><a style="text-decoration:none;" href="https://youtu.be/Z-mdnY2ObRg" class="link-light">p-n junction diode</a></li>
+          <li><a style="text-decoration:none;" href="https://youtu.be/85KspLpuoXU" class="link-light">Zener diode, LED, Solar Cell</a></li>
+        </ul>
+        </ol>
+    </div>
+    <div class="div3">
+        <section>BEE</section><hr>
+        <ol>
+            <li>BEE Complete(Not from our college)</li>
+            <ul>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrQZZoeQJGYojStGLstMnffL" class="link-light">DC CIRCUITS</a></li>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrQZP4zxS47bzkf8sGYlaIKM" class="link-light">AC CIRCUIT</a></li>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrSS6_Mk0UbOySQzDBY-uHJQ" class="link-light">THREE PHASE CIRCUIT</a></li>
+            </ul>
+        </ol>
+    </div>
+    <div class="div4">
+        <section>Mathematics</section><hr>
+        <ol>
+            <li>Maths(Not from our college)</li>
+            <ul>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrSsOQDQOpYrDUp-4zQO8YFX" class="link-light">COMPLEX NUMBERS 1.O</a></li>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrS_-g4FAl-URADpCSOes1bg" class="link-light">COMPLEX NUMBERS 2.O</a></li>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrT-Gy4k0r8jAr0mX2K32yyV" class="link-light">MATRICES 1.0</a></li>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrRE3XV3AqXScC2Dw8Qf0oWt" class="link-light">MATRICES 2.O</a></li>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrQf7IBhKlsnLXwJRZSDMhun" class="link-light">PARTIAL DIFFERENTIATION</a></li>
+                <li><a style="text-decoration: none;" href="https://youtube.com/playlist?list=PLKS7ZMKnbPrQDCA9bd-HG5VrGxW4ZZfLP" class="link-light">SUCCESSIVE DIFFERENTIATION (LEBNITZ)</a></li>
+            </ul>
+        </ol>
+    </div>`;
+    document.getElementById("pagestyle").setAttribute('href', 'assets/lectures.css');
+}
+
+function groups(){
+    document.getElementById("incont").innerHTML = `<div><div class="div1">
+	<section><i class="bi bi-whatsapp"></i> Whatsapp Groups</section><hr class="link-dark">
+	<ul class="link-dark">
+		<li><a style="text-decoration: none;" href="https://chat.whatsapp.com/DyDbUxoQv1H1KHZbEAkQML" class="link-dark">NOTE-SHARING KJSIEIT</a></li>
+		<li><a style="text-decoration: none;" href="https://chat.whatsapp.com/DJci8Mxgn1PHTJGJpfq2LH" class="link-dark">KJSIEIT katta ☕</a></li>
+	</ul>
+</div>
+<div class="div2">
+	<section class="link-dark"><i class="bi bi-instagram"></i> Instagram Pages</section><hr class="link-dark">
+	<ul class="link-dark">
+		<li><a style="text-decoration: none;" href="https://www.instagram.com/kjsieit_25/?utm_medium=copy_link" class="link-dark">Instagram Official Page</a></li>
+	</ul>	
+</div>
+<div class="div3">
+	<section class="link-dark"><i class="bi bi-telegram"></i> Telegram Groups</section><hr class="link-dark">
+	<ul class="link-dark">
+		<li><a style="text-decoration: none;" href="https://t.me/+OfiB0LrrpQ0yNzBl" class="link-dark">KJSIEIT</a></li>
+		<li><a style="text-decoration: none;" href="https://t.me/codingcommunity01" class="link-dark">CODING COMMUNITY</a></li>
+	</ul>
+</div>
+<div class="div4">
+	<section class="link-dark"><i class="bi bi-discord"></i> Discord Group</section><hr class="link-dark">
+	<ul class="link-dark">
+		<li><a style="text-decoration: none;" href="https://discord.gg/2nhcHnPf" class="link-dark">Somaiyaaaaaa</a></li>
+		<li><a style="text-decoration: none;" href="https://discord.gg/Uk39pf92Ek" class="link-dark">KJSIEIT STUDENTS</a></li>
+	</ul>
+</div></div>`;
+    document.getElementById("pagestyle").setAttribute('href', 'assets/groups.css');
+}
+
+function home(){
+    document.getElementById("incont") = `<div><div class="div1">
+    <section class="link-light">About</section><hr class="link-light">
+    <p class="link-light">Hey, I'm Dhruv Chheda from FYComp KJSIEIT. I built this website so that all the study material would be easily accessible to all students.  This website contains Notes, Lecture Links, Group Links, Time Tables, Syllabus and Submission dates of assignments and experiments. I Thank all of you who helped me in the making process. Feedbacks are always welcomed. If anybody has any ideas for advancement for the website, you can dm me the same.</p>
+  </div>
+  <div class="footer">
+    <footer>
+      <div class="container">
+      <div class="sec aboutus">
+        <h2>About Me</h2>
+        <p>I have mentioned some of my social media accounts below.</p>
+        <ul class="sci">
+          <li><a href="https://www.facebook.com/dhruv.chheda.33"><i class="bi bi-facebook"></i></a></li>
+          <li><a href="https://www.instagram.com/chheda.dhruv3/"><i class="bi bi-instagram"></i></a></li>
+          <li><a href="https://twitter.com/ChhedaDhruv4"><i class="bi bi-twitter"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/dhruv-chheda-52637522a/"><i class="bi bi-linkedin"></i></a></li>
+          <li><a href="https://github.com/chhedadhruv"><i class="bi bi-github"></i></a></li>
+        </ul>
+      </div>
+      <div class="sec quickLinks">
+        <h2>Quick Links</h2>
+        <ul>
+          <li><a href="https://kjsieit.somaiya.edu.in/en">KJSIEIT official website</a></li>
+        </ul>
+      </div>
+      <div class="sec contact">
+        <h2>Contact Info</h2>
+      <ul class="info">
+        <li>
+          <span><i class="bi bi-envelope-fill"></i></span>
+          <p><a href="mailto:chhedadhruv1234@gmail.com">Email me</a></p>
+        </li>
+      </ul>
+      </div>
+      </div>
+    </footer>
+  </div></div>`;
+    document.getElementById("pagestyle").setAttribute('href', 'assets/styles.css');
+}
+
+    
